751cbbed58f58c35511ed3a5e146943ba8ccd991
samme1210 <samme1210@gmail.com>
2023-04-14 21:48:26 +0100

    added working firebase functions

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
 import React, { useState } from "react";
 import "./App.css";
-import { auth } from "./firebase/init";
+import { auth, db } from "./firebase/init";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  doc,
+  query,
+  where,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -10,17 +21,73 @@ import {
 import Nav from "./components/Nav";
 
 function App() {
-
   const [user, setUser] = React.useState({});
-  const [loading, setLoading] = React.useState(true)
+  const [loading, setLoading] = React.useState(true);
+
+  // Create Data
+
+  function createPost() {
+    const post = {
+      title: "Start Online Cunsultancy Business",
+      description: "Do Frontend Simplified",
+      uid: user.uid,
+    };
+    addDoc(collection(db, "posts"), post);
+  }
+
+  // Read Data
+
+  async function getAllPosts() {
+    const { docs } = await getDocs(collection(db, "posts"));
+    //Turns array into useable javascript
+    const posts = docs.map((elem) => ({ ...elem.data(), id: elem.id }));
+    console.log(posts);
+  }
+
+  async function getPostByID(id) {
+    const postRef = doc(db, "posts", id);
+    const postSnap = await getDoc(postRef);
+    return postSnap.data();
+  }
+
+  async function getPostsByUid() {
+    const postCollectionRef = await query(
+      collection(db, "posts"),
+      where("uid", "==", user.uid)
+    );
+    const { docs } = await getDocs(postCollectionRef);
+    console.log(docs.map((doc) => doc.data()));
+  }
+
+  // Update Data
+
+  async function updatePost(id) {
+    const hardCoadedId = "2JX0aE4XktQ4KPpFlKHq";
+    const postRef = doc(db, "posts", hardCoadedId);
+    const post = await getPostByID(hardCoadedId);
+    const newPost = {
+      ...post,
+      title: "Land a £500k job",
+    };
+    updateDoc(postRef, newPost);
+    console.log(newPost);
+  }
+
+  // Delete Data
+
+  function deletePost() {
+    const hardCoadedId = "2JX0aE4XktQ4KPpFlKHq";
+    const postRef = doc(db, "posts", hardCoadedId);
+    deleteDoc(postRef);
+  }
 
   React.useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       setLoading(false);
       if (user) {
-        setUser(user)
+        setUser(user);
       }
-    })
+    });
   }, []);
 
   function register() {
@@ -35,13 +102,13 @@ function App() {
 
   function login() {
     signInWithEmailAndPassword(auth, "email@email.com", "test123")
-    .then(({ user }) => {
-      console.log(user);
-      setUser(user)
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+      .then(({ user }) => {
+        console.log(user);
+        setUser(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   function logout() {
@@ -50,8 +117,32 @@ function App() {
   }
   return (
     <div className="App">
-      <Nav login={login} register={register} logout={logout} user={user} loading={loading}/>
+      <Nav
+        login={login}
+        register={register}
+        logout={logout}
+        user={user}
+        loading={loading}
+      />
       <h5>{user.email}</h5>
+      <button className="create__post btn" onClick={createPost}>
+        Create Post
+      </button>
+      <button className="create__post btn" onClick={getAllPosts}>
+        Get all posts
+      </button>
+      <button className="create__post btn" onClick={getPostByID}>
+        Get post by ID
+      </button>
+      <button className="create__post btn" onClick={getPostsByUid}>
+        Get post by Uid
+      </button>
+      <button className="create__post btn" onClick={updatePost}>
+        Update post
+      </button>
+      <button className="create__post btn" onClick={deletePost}>
+        Delete post
+      </button>
     </div>
   );
 }
